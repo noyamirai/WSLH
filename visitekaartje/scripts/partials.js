24afbc4a1db311c8acfa353ec1aa529e7096ecaf
noyamirai <planet1109@hotmail.com>
2023-02-09 15:34:19 +0100

    JAAAAAAA MOBILE ARTICLE APPEND

--- visitekaartje/scripts/partials.js
+++ visitekaartje/scripts/partials.js
@@ -28,6 +28,43 @@ const setCommentHeading = (headingContainer) => {
 
 }
 
+const getPercentageBars = (percentage) => {
+
+    let result = '';
+    const nPercentage = percentage/10;
+
+    for (let i = 1; i <= 10; i++) {
+
+        if (i <= nPercentage) {
+            if (i == 1)
+                result += '<span class="green">';
+
+            result += '=';
+
+            if (i == nPercentage)
+                result += '</span>';
+        } else {
+            result += '-';
+        }
+    }
+
+    return result;
+}
+
+const setPercentageBars = () => {
+    const skillIndicators = document.querySelectorAll('.js-skill-level-container');
+
+    skillIndicators.forEach(container => {
+        const levelBar = container.querySelector('.js-level');
+        const assignedLevel = parseInt(container.dataset.percentage);
+        const levelBarContent = getPercentageBars(assignedLevel);
+        const percentageText = container.nextElementSibling;
+
+        percentageText.innerHTML = assignedLevel + '%';
+        levelBar.innerHTML = levelBarContent; 
+    });
+}
+
 const setPromptPartial = () => {
 
     const allPromptInputs = document.querySelectorAll('form');
@@ -56,7 +93,13 @@ const setPromptPartial = () => {
 
 const insertPromptPartial = async () => {
     const promptHtml = setPromptPartial();
-    articleElement.innerHTML += promptHtml;
+
+    let htmlObject = document.createElement('div');
+    htmlObject.innerHTML = promptHtml;
+    articleElement.appendChild(htmlObject);
+
+    // CAUSES JUMPING
+    // articleElement.innerHTML += promptHtml;
 }
 
 const getSectionHeading = (sectionId) => {
@@ -65,25 +108,86 @@ const getSectionHeading = (sectionId) => {
         'help': 'Command list',
         'about': 'Get to know me',
         'projects': 'Projects I am proud of',
-        'skills': 'Things Im good at'
+        'skills': 'Things Im good at',
+        'contact': 'Contact'
     }
 
     return headings[sectionId];
 }
 
+const getSkills = () => {
+
+    const skillData = [
+        {
+            'title': 'HTML',
+            'percentage': '80'
+        },
+        {
+            'title': 'CSS',
+            'percentage': '60'
+        },
+        {
+            'title': 'Javascript',
+            'percentage': '80'
+        },
+        {
+            'title': 'Node.js',
+            'percentage': '50'
+        },
+        {
+            'title': 'PHP',
+            'percentage': '50'
+        },
+        {
+            'title': 'SQL',
+            'percentage': '50'
+        }
+    ];
+
+    let skillHtml = '<ul class="skills">';
+
+    skillData.forEach(skillItem => {
+        
+        skillHtml += '<li class="skill__item"><p>' + skillItem.title + '</p>';
+        skillHtml += '<div class="skill__level"><span class="skill__level__indicator js-skill-level-container" data-percentage="' + skillItem.percentage +'"><span class="blue">[</span>';
+
+        const levelBarContent = getPercentageBars(skillItem.percentage);
+
+        skillHtml += '<span class="js-level">' + levelBarContent + '</span>';
+        skillHtml += '<p class="skill__level__percentage js-level-percentage">' + skillItem.percentage + '%</p>';
+        skillHtml += '<span class="blue">]</span>';
+
+        skillHtml += '</div> </li>';
+    });
+
+
+    return skillHtml;
+}
+
 const getContent = (sectionId) => {
 
     const content = {
-        'help': '<ul class="generic-list generic-list--help"><li class="list__item"><h3>help</h3></li><li class="list__item"><h3>about</h3></li><li class="list__item"><h3>projects</h3></li><li class="list__item"><h3>skills</h3></li></ul>',
-        'about': '<p  id="text">&gt;_Hello! I guess you&#39;re here to read a little more about who I am and what I do.</p><p >&gt;_My name is Maijla Ikiz, I am a 24 y/o <s>nerd</s> full-stack web developer + designer by day and average video game enthusiast by night :)</p><p >&gt;_I am currently a third year Communication &amp; Multimedia Design student at the Amsterdam University of Applied Sciences. In the meantime I have also been working as a part-time web developer @ <a target="_blank" title="https://activo.nl" href="https://activo.nl">Activo</a> for over 4 years now.</p>',
-        'projects': '<ul class="generic-list"> <li class="list__item"> <a href="#"> <h3>&gt;_CMD Online <span>~A browser based matching application that helps teachers of CMD create well-balanced project teams based on skills of students.</span></h3> </a> <ul class="list__item__tags"> <li class="tag tag--yellow"> GDA 2022 Nominee </li> <li class="tag tag--red"> HTML </li> <li class="tag tag--purple"> CSS </li> <li class="tag tag--blue"> JS </li> <li class="tag tag--green"> Node.js </li> </ul> </li> <li class="list__item"> <a href="#"> <h3>&gt;_CMD Online <span>~A browser based matching application that helps teachers of CMD create well-balanced project teams based on skills of students.</span></h3> </a> <ul class="list__item__tags"> <li class="tag tag--yellow"> GDA 2022 Nominee </li> <li class="tag tag--red"> HTML </li> <li class="tag tag--purple"> CSS </li> <li class="tag tag--blue"> JS </li> <li class="tag tag--green"> Node.js </li> </ul> </li> <li class="list__item"> <a href="#"> <h3>&gt;_CMD Online <span>~A browser based matching application that helps teachers of CMD create well-balanced project teams based on skills of students.</span></h3> </a> <ul class="list__item__tags"> <li class="tag tag--yellow"> GDA 2022 Nominee </li> <li class="tag tag--red"> HTML </li> <li class="tag tag--purple"> CSS </li> <li class="tag tag--blue"> JS </li> <li class="tag tag--green"> Node.js </li> </ul> </li> </ul>',
-        'skills': '<ul class="skills"> <li class="skill__item"> <p>HMTL</p> <div class="skill__level"> <span class="skill__level__indicator js-skill-level-container" data-percentage="80"> <span class="blue">[</span> <span class="js-level">----------</span> <span class="blue">]</span> </span> <p class="skill__level__percentage js-level-percentage">0%</p> </div> </li> <li class="skill__item"> <p>CSS</p> <div class="skill__level"> <span class="skill__level__indicator js-skill-level-container" data-percentage="60"> <span class="blue">[</span> <span class="js-level">----------</span> <span class="blue">]</span> </span> <p class="skill__level__percentage js-level-percentage">0%</p> </div> </li> <li class="skill__item"> <p>Javascript</p> <div class="skill__level"> <span class="skill__level__indicator js-skill-level-container" data-percentage="80"> <span class="blue">[</span> <span class="js-level">----------</span> <span class="blue">]</span> </span> <p class="skill__level__percentage js-level-percentage">0%</p> </div> </li> <li class="skill__item"> <p>Node.js</p> <div class="skill__level"> <span class="skill__level__indicator js-skill-level-container" data-percentage="50"> <span class="blue">[</span> <span class="js-level">----------</span> <span class="blue">]</span> </span> <p class="skill__level__percentage js-level-percentage">0%</p> </div> </li> <li class="skill__item"> <p>PHP</p> <div class="skill__level"> <span class="skill__level__indicator js-skill-level-container" data-percentage="50"> <span class="blue">[</span> <span class="js-level">----------</span> <span class="blue">]</span> </span> <p class="skill__level__percentage js-level-percentage">0%</p> </div> </li> <li class="skill__item"> <p>SQL</p> <div class="skill__level"> <span class="skill__level__indicator js-skill-level-container" data-percentage="50"> <span class="blue">[</span> <span class="js-level">----------</span> <span class="blue">]</span> </span> <p class="skill__level__percentage js-level-percentage">0%</p> </div> </li> </ul>'
+        'help': '<ul class="generic-list generic-list--help"><li class="list__item"><h3>help</h3></li><li class="list__item"><h3>about</h3></li><li class="list__item"><h3>projects</h3></li><li class="list__item"><h3>skills</h3></li><li class="list__item"><h3>contact</h3></li><li class="list__item"><h3>clear</h3></li></ul>',
+        'projects': '<ul class="generic-list"> <li class="list__item"> <a href="https://www.cmd-amsterdam.nl/portfolio/cmd-online/"> <h3>&gt;_CMD Online <span>~A browser based matching application that helps teachers of CMD create well-balanced project teams based on skills of students.</span></h3> </a> <ul class="list__item__tags"> <li class="tag tag--yellow"> GDA 2022 Nominee </li> <li class="tag tag--red"> HTML </li> <li class="tag tag--purple"> CSS </li> <li class="tag tag--blue"> JS </li> <li class="tag tag--green"> Node.js </li> </ul> </li> <li class="list__item"> <h3>&gt;_Activibes <span>~A tool used at work to let other know how you&#39;re feeling everyday in order to improve team awareness and closeness.</span></h3> <ul class="list__item__tags"> <li class="tag tag--red"> HTML </li> <li class="tag tag--purple"> CSS </li> <li class="tag tag--blue"> JS </li> <li class="tag tag--green"> Node.js </li> </ul> </li> <li class="list__item"> <h3>&gt;_Actiscan <span>~Web application that helps standholders scan leads via QR codes and form connections while being at an event.</span></h3> <ul class="list__item__tags"> <li class="tag tag--red"> HTML </li> <li class="tag tag--purple"> CSS </li> <li class="tag tag--blue"> JS </li> <li class="tag tag--green"> Node.js </li> </ul> </li> <li class="list__item"> <a href="https://www.cmd-amsterdam.nl/portfolio/hacking-simulator-1-0/" target="_blank"> <h3>&gt;_Hacking Simulator <span>~A Cyberpunk 2077 inspired “simulator” I made in JS for a school assignment.</span></h3> </a> <ul class="list__item__tags"> <li class="tag tag--yellow"> GDA 2021 Nominee </li> <li class="tag tag--red"> HTML </li> <li class="tag tag--purple"> CSS </li> <li class="tag tag--blue"> JS </li> <li class="tag tag--green"> Node.js </li> </ul> </li> </ul>',
+        'contact': '<p>&gt;_Want to get in touch? Cool! You can contact me by sending me an <a href="mailto:planet1109@hotmail.com" target="_blank">e-mail</a>, and I will get back to you as soon as possible.</p> <p>&gt;_You might also be curious about my socials as well. Don&#39;t worry, I&#39;ve got you covered!</p> <ul class="generic-list generic-list--link"> <li class="list__item"> <a href="https://www.linkedin.com/in/maijla-ikiz-775bab178/" target="_blank"> <h3>LinkedIn</h3> </a> </li> <li class="list__item"> <a href="https://github.com/noyamirai" target="_blank"> <h3>Github</h3> </a> </li> </ul>'
     }
 
     return content[sectionId];
 }
 
-const setSectionPartial = (sectionId) => {
+const fetchAboutContent = async () => {
+    const corsTrick = 'https://cors-anywhere.herokuapp.com/';
+    const url = 'https://whois.fdnd.nl/api/v1/member?id=cldepgz223wz20bw5mmlfm503';
+
+    let data = await fetch(corsTrick + url);
+    data = await data.json();
+
+    if (data) {
+        return data;
+    }
+}
+
+const setSectionPartial = async (sectionId) => {
 
     const allSpecificSections = document.querySelectorAll('.js-' + sectionId);
 
@@ -97,24 +201,62 @@ const setSectionPartial = (sectionId) => {
     hashtagString += '####';
 
     let sectionHtml = '<section class="info js-' + nId + '"> <div class="info__heading comment"> <span class="comment__text comment__hashtags">' + hashtagString  + '</span> <h2 class="comment__text comment__heading">' + heading + '</h2> <span class="comment__text comment__hashtags">' + hashtagString + '</span> </div><div class="info__content">'
-    sectionHtml += getContent(sectionId);
-    sectionHtml += '</div></section>';
+    
+    // fetch
+    if (sectionId == 'about') {
 
+        // TODO: error handling
+        try {
+            let result = await fetchAboutContent();
+
+            sectionHtml += result.member.bio.html;
+            sectionHtml += '</div></section>';
+            return sectionHtml;
+
+        } catch (error) {
+            console.log(error);
+            return;
+        }
+            
+    } else if (sectionId == 'skills') {
+        sectionHtml += getSkills();
+
+    } else {
+        sectionHtml += getContent(sectionId);
+    }
+
+    sectionHtml += '</div></section>';
     return sectionHtml;
 
 };
 
-const revealSection = (sectionId) => {
+const revealSection = async (sectionId) => {
 
-    const sectionHtml = setSectionPartial(sectionId);
-    articleElement.innerHTML += sectionHtml;
+    try {
+
+        const sectionHtml = await setSectionPartial(sectionId);
+
+        let htmlObject = document.createElement('div');
+        htmlObject.innerHTML = sectionHtml;
+        articleElement.appendChild(htmlObject);
+        // articleElement.innerHTML = sectionHtml;
+
+        return true;
+
+    } catch (error) {
+        return false;
+    }
 
 }
 
 const showError = () => {
 
-    let errorHtml = '<section class="info info--message error"><p>command not found. type "help" to see available commands.</p></section>';
-    articleElement.innerHTML += errorHtml;
+    let errorHtml = '<p>command not found. type "help" to see available commands.</p></section>';
+
+    let htmlObject = document.createElement('section');
+    htmlObject.classList.add(['info', 'info--message', 'error'])
+    htmlObject.innerHTML = errorHtml;
+    articleElement.appendChild(htmlObject);
 
     insertPromptPartial();
 }
--- visitekaartje/scripts/utils.js
+++ visitekaartje/scripts/utils.js
@@ -51,3 +51,128 @@ const getCurrentTime = () => {
 
     return currentTime;
 }
+
+ async function startType(string, textEl, typed, index, skip = false) {
+    return new Promise(resolve => {
+
+        if (index < string.length) {
+            // console.log('typing!');
+
+            const char = string.charAt(index);
+            typed += char;
+
+            let resultObj = {
+                finished: false,
+                string: string,
+                typed: typed,
+                nIndex: index,
+                skip: false
+            }
+
+            // moving onto next char (complete html entity)
+            if (skip && char != ';') {
+                // console.log('STILL HTML ENTITY');
+                index++;
+                resultObj.skip = true;
+                resultObj.nIndex = index;
+
+                resolve(resultObj);
+                return;
+            }
+
+            // If its HTML entity -> don't display until its completed (;)
+            if (char == '&') {
+                // console.log('HTML ENTITY');
+                index++;
+                resultObj.skip = true;
+                resultObj.nIndex = index;
+
+                resolve(resultObj);
+                return;
+            }
+
+            // console.log('NORMAL TEXT/END OF ENTITY');
+
+            textEl.innerHTML = typed;
+            index++;
+
+            resultObj.nIndex = index;
+            resultObj.skip = false;
+
+            setTimeout(function() {
+                resolve(resultObj)
+            }, 75);
+        }
+
+        if (index == string.length) {
+            // console.log('done typing, go next');
+            resolve({finished:true})
+            return;                
+        }
+
+    })
+}
+
+async function processParagraphs(paragraphs, index) {
+    // console.log('attempting paragraph index: ' + index);
+    // console.log(index);
+    // console.log(paragraphs.length);
+
+    // All elements have been animated
+    if (index >= paragraphs.length) {
+        // console.log('DONE PROCESSING');
+        return true;
+    }
+
+    // console.log('we have permission for do-while!');
+
+    const paragraph = paragraphs[index];
+    paragraph.classList.remove('hide');
+
+    let result;  
+
+    // Will continue calling startType() until resolve has finished:true
+    do {
+        // console.log('attempt...');
+        // console.log(result);
+        // console.log((result && result.skip ? true : false));
+        // console.log('startType()');
+        
+        result = await startType(
+            (result && result.string ? result.string : paragraph.innerHTML),
+            paragraph, 
+            (result && result.typed ? result.typed: ''), 
+            (result && result.nIndex ? result.nIndex: 0), 
+            (result && result.skip ? true : false)
+        );
+
+    } while (!result.finished);
+
+    // Finished
+    // console.log('FINISHEDDDD');
+    // console.log(result);
+
+    // Recursion
+    index++;
+    return processParagraphs(paragraphs, index);
+}
+
+// SOURCE: chat.openai.com/chat
+const logVisit = () => {
+    // Get the current date and time
+    let currentDate = new Date();
+
+    // Convert the date and time to a string in the desired format
+    let dateString = currentDate.toLocaleString('en-US', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+
+    // Save the date and time to local storage
+    localStorage.setItem("lastVisit", dateString);
+    return true;
+}
+
+const retrieveLastVisit = () => {
+    let lastVisit = localStorage.getItem("lastVisit");
+
+    // Display the date and time
+    return lastVisit;
+}
